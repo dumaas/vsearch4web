51e6589c489792e244b20983867d8951593ae144
dumaas <normify@gmail.com>
2020-05-02 11:45:56 -0500

    Added ConnectionError support for '/viewlog'.

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
 
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError
 from checker import check_logged_in
 
 app = Flask(__name__)
 
-app.config["dbconfig"] = {"host": "127.0.0.1",
-                          "user": "vsearch",
-                          "password": "vsearchpasswd",
-                          "database": "vsearchlogDB", }
+app.config["dbconfig"] = {
+    "host": "127.0.0.1",
+    "user": "vsearch",
+    "password": "vsearchpasswd",
+    "database": "vsearchlogDB",
+}
 
 
 @app.route("/login")
@@ -32,11 +34,16 @@ def log_request(req: "flask_request", res: str) -> None:
                   (phrase, letters, ip, browser_string, results)
                   values
                   (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form["phrase"],
-                              req.form["letters"],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+        cursor.execute(
+            _SQL,
+            (
+                req.form["phrase"],
+                req.form["letters"],
+                req.remote_addr,
+                req.user_agent.browser,
+                res,
+            ),
+        )
 
 
 @app.route("/search4", methods=["POST"])
@@ -45,34 +52,46 @@ def do_search() -> "html":
     letters = request.form["letters"]
     title = "Here are your results:"
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print("***** Logging failed with this error:", str(err))
     return render_template(
         "results.html",
         the_title=title,
         the_results=results,
         the_phrase=phrase,
-        the_letters=letters,)
+        the_letters=letters,
+    )
 
 
 @app.route("/")
 @app.route("/entry")
 def entry_page() -> "html":
-    return render_template("entry.html", 
-                           the_title="Welcome to search4letters on the web!")
+    return render_template(
+        "entry.html", the_title="Welcome to search4letters on the web!"
+    )
 
 
 @app.route("/viewlog")
 @check_logged_in
 def view_the_log() -> "html":
     """Display the contents of the log file as a HTML table."""
-    with UseDatabase(app.config["dbconfig"]) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results
-                  from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
+    try:
+        with UseDatabase(app.config["dbconfig"]) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results
+                      from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+    except ConnectionError as err:
+        print("Is your database switched on? Error:", str(err))
+    except Exception as err:
+        print("Something went wrong:", str(err))
+    return "Error"
     titles = ("Phrase", "Letters", "Remote_addr", "User_agent", "Results")
     return render_template(
-        "viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents,)
+        "viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents,
+    )
 
 
 if __name__ == "__main__":
