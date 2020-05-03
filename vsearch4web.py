fbce3f6b7a7c3dddef016fabdd49685c2bf3a39f
dumaas <normify@gmail.com>
2020-05-02 21:16:55 -0500

    Increased performance with by threading write-to-database.

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, escape, session
+from threading import Thread
+from flask import (
+    Flask,
+    render_template,
+    request,
+    escape,
+    session,
+    copy_current_request_context,
+)
 from vsearch import search4letters
 
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
@@ -26,34 +34,35 @@ def do_logout() -> str:
     return "You are now logged out."
 
 
-def log_request(req: "flask_request", res: str) -> None:
-    """Log details of the web request and the results."""
-
-    with UseDatabase(app.config["dbconfig"]) as cursor:
-        _SQL = """insert into log
-                  (phrase, letters, ip, browser_string, results)
-                  values
-                  (%s, %s, %s, %s, %s)"""
-        cursor.execute(
-            _SQL,
-            (
-                req.form["phrase"],
-                req.form["letters"],
-                req.remote_addr,
-                req.user_agent.browser,
-                res,
-            ),
-        )
-
-
 @app.route("/search4", methods=["POST"])
 def do_search() -> "html":
+    @copy_current_request_context
+    def log_request(req: "flask_request", res: str) -> None:
+        """Log details of the web request and the results."""
+
+        with UseDatabase(app.config["dbconfig"]) as cursor:
+            _SQL = """insert into log
+                      (phrase, letters, ip, browser_string, results)
+                      values
+                      (%s, %s, %s, %s, %s)"""
+            cursor.execute(
+                _SQL,
+                (
+                    req.form["phrase"],
+                    req.form["letters"],
+                    req.remote_addr,
+                    req.user_agent.browser,
+                    res,
+                ),
+            )
+
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     title = "Here are your results:"
     results = str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print("***** Logging failed with this error:", str(err))
     return render_template(
@@ -83,6 +92,13 @@ def view_the_log() -> "html":
                       from log"""
             cursor.execute(_SQL)
             contents = cursor.fetchall()
+        titles = ("Phrase", "Letters", "Remote_addr", "User_agent", "Results")
+        return render_template(
+            "viewlog.html",
+            the_title="View Log",
+            the_row_titles=titles,
+            the_data=contents,
+        )
     except ConnectionError as err:
         print("Is your database switched on? Error:", str(err))
     except CredentialsError as err:
@@ -92,11 +108,9 @@ def view_the_log() -> "html":
     except Exception as err:
         print("Something went wrong:", str(err))
     return "Error"
-    titles = ("Phrase", "Letters", "Remote_addr", "User_agent", "Results")
-    return render_template(
-        "viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents,
-    )
 
+
+app.secret_key = "YouWillNeverGuessMySecretKey"
 
 if __name__ == "__main__":
     app.run(debug=True)
